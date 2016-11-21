eee3f7b70c04c209fda59babddb0c17fc6f8e282
user <who@where.com>
2016-11-21 09:07:28 +0800

    v2

---
+++ app/src/main/java/com/org/iii/android10/MainActivity.java
@@ -0,0 +1,40 @@
+package com.org.iii.android10;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private SharedPreferences sp;
+    private SharedPreferences.Editor editor;
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = (TextView)findViewById(R.id.textTv);
+        sp = getSharedPreferences("gamedata",MODE_PRIVATE);
+        editor = sp.edit();
+
+    }
+
+    // 偏好設定 => save
+    public void test1(View v){
+        editor.putInt("stage", 3);
+        editor.putString("user", "Brad");
+        editor.commit();
+        Toast.makeText(this, "Save OK", Toast.LENGTH_SHORT).show();
+    }
+    // 偏好設定 => read.
+    public void test2(View v){
+        int stage = sp.getInt("stage",0);
+        String name = sp.getString("user","nobody");
+        tv.setText("Stage: " + stage + "\n" +
+                "User: " + name);
+    }
+}
